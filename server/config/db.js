faa90d7a0408a4316d75325b1d8323767208cb02
robinsali <robinsalimaredia@gmail.com>
2022-10-31 15:47:25 -0400

    Added JWT

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
     // URI: "mongodb://127.0.0.1:27017/contact_collections",
     URI: "mongodb+srv://Robins:Mumbai%40123@personalportfolio.rrqok16.mongodb.net/contact_collections?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
   };
   
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,10 @@ let router = express.Router();
 let mongoose = require("mongoose");
 let passport = require("passport");
 
+// enable jwt
+let jwt = require('jsonwebtoken');
+let DB = require('../config/db');
+
 //create the user model instance
 let userModel = require("../models/user");
 let User = userModel.User; //alias
@@ -71,6 +75,17 @@ module.exports.processLoginPage = (req, res, next) => {
       if (err) {
         return next(err);
       }
+      const payload = 
+            {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }
+
+            const authToken = jwt.sign(payload, DB.Secret, {
+                expiresIn: 604800 // 1 week
+            });
       return res.redirect("/book-list");
     });
   })(req, res, next);
